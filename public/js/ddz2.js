b77115e1c0c5c0eda4bbac18be0b09e14563af3b
Jack_Lai <282753932@qq.com>
2020-03-05 22:21:04 +0800

    完美适配移动端强制横屏

--- public/js/ddz2.js
+++ public/js/ddz2.js
@@ -1,12 +1,24 @@
 window.addEventListener('orientationchange', function () {
+    console.log(`状态改变`);
     const width = document.documentElement.clientWidth;
     const height = document.documentElement.clientHeight;
     const contentDOM = document.getElementById('out');
     if (width < height) { // 竖屏
-        contentDOM.style.width = height + 'px';
-        contentDOM.style.height = width + 'px';
-        contentDOM.style.top = (height - width) / 2 + 'px';
+        // contentDOM.style.width = height + 'px';
+        // contentDOM.style.height = '94vw';
+        // contentDOM.style.transform = 'rotate(90deg)';
+        contentDOM.style.top = (height - width) / 2 + 10 + 'px';
         contentDOM.style.left = 0 - (height - width) / 2 + 'px';
-        contentDOM.style.transform = 'rotate(90deg)';
+        // contentDOM.style.position = 'absolute';
     }
 })
+function init2(){
+    const width = document.documentElement.clientWidth;
+    const height = document.documentElement.clientHeight;
+    const contentDOM = document.getElementById('out');
+    if (width < height) { // 竖屏
+        contentDOM.style.top = (height - width) / 2 + 10 + 'px';
+        contentDOM.style.left = 0 - (height - width) / 2 + 'px';
+    }
+}
+init2()
